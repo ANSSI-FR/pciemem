c31c72a53b3f82593702a5e1712c8c70039d71c4
Yves-Alexis Perez <yves-alexis.perez@ssi.gouv.fr>
2017-08-02 15:58:58 +0200

    add pciemem source file based on usbmem

---
+++ src/pciemem.c
@@ -0,0 +1,560 @@
+#include <linux/module.h>
+#include "usbmem.h"
+
+MODULE_LICENSE("GPL");
+
+/* USB part */
+
+static void plx_delete(struct kref *kref)
+{
+	struct usb_plx *dev = to_plx_dev(kref);
+
+	usb_put_dev(dev->udev);
+	kfree(dev->bulk_in_buffer);
+	kfree(dev->bulk_out_buffer);
+	kfree(dev);
+}
+
+static int usbmem_open(struct inode *inode, struct file *file)
+{
+	struct usb_plx *dev;
+	struct usb_interface *interface;
+	int subminor;
+	int retval = 0;
+	pr_debug("open usbmem\n");
+
+	subminor = iminor(inode);
+
+	interface = usb_find_interface(&plx_driver, subminor);
+	if (!interface) {
+		pr_err("%s - error, can't find device for minor %d\n",
+			__func__, subminor);
+		retval = -ENODEV;
+		goto exit;
+	}
+
+	dev = usb_get_intfdata(interface);
+	if (!dev) {
+		retval = -ENODEV;
+		goto exit;
+	}
+
+	retval = usb_autopm_get_interface(interface);
+	if (retval)
+		goto exit;
+
+	/* increment our usage count for the device */
+	kref_get(&dev->kref);
+
+	/* save our object in the file's private structure */
+	file->private_data = dev;
+
+	pr_debug("opened usbmem\n");
+
+exit:
+	return retval;
+}
+
+static int usbmem_release(struct inode *inode, struct file *file)
+{
+	struct usb_plx *dev;
+
+	dev = file->private_data;
+	if (dev == NULL)
+		return -ENODEV;
+
+	/* allow the device to be autosuspended */
+	mutex_lock(&dev->io_mutex);
+	if (dev->interface)
+		usb_autopm_put_interface(dev->interface);
+	mutex_unlock(&dev->io_mutex);
+
+	/* decrement the count on our device */
+	kref_put(&dev->kref, plx_delete);
+
+	pr_debug("released usbmem\n");
+	return 0;
+}
+
+static int usbmem_flush(struct file *file, fl_owner_t id)
+{
+	struct usb_plx *dev;
+	int res;
+
+	dev = file->private_data;
+	if (dev == NULL)
+		return -ENODEV;
+
+	/* wait for io to stop */
+	mutex_lock(&dev->io_mutex);
+	plx_draw_down(dev);
+
+	/* read out errors, leave subsequent opens a clean slate */
+	spin_lock_irq(&dev->err_lock);
+	res = dev->errors ? (dev->errors == -EPIPE ? -EPIPE : -EIO) : 0;
+	dev->errors = 0;
+	spin_unlock_irq(&dev->err_lock);
+
+	mutex_unlock(&dev->io_mutex);
+
+	return res;
+}
+
+static int plx_readreg(struct usb_plx *dev, u16 reg)
+{
+	int ret = -EINVAL;
+	u32 csrctl = 0U; // Format p362
+	u32 *buf;
+	int tlen = 0;
+	int pipe;
+
+  csrctl |= 0b01 << 4; // select USB configuration registers
+  csrctl |= 1 << 6; // CSR Start
+  csrctl |= 1 << 7; // CSR Read transaction
+  csrctl |= (uint32_t ) reg << 16; // destination address
+
+	buf = kmalloc(4, GFP_KERNEL);
+	if (!buf) {
+		pr_err("Can't allocate memory for USB buffer");
+		return -ENOMEM;
+	}
+	memcpy(buf, &csrctl, 4);
+
+	pipe = usb_sndbulkpipe(dev->udev, dev->csr_out_endpointAddr);
+	ret = usb_bulk_msg(dev->udev, pipe, buf, 4, &tlen, 1000);
+
+	if (ret != 0 || tlen != 4) {
+		pr_warn("Read reg USB out transfer failed: ret = %d, tlen = %d\n", ret, tlen);
+		return ret;
+	}
+
+	memset(buf, 0, 4);
+	ret = usb_bulk_msg(dev->udev,
+			usb_rcvbulkpipe(dev->udev, dev->csr_in_endpointAddr),
+			buf, 4, &tlen, 1000);
+	if (ret != 0 || tlen != 4)
+		pr_warn("USB in  transfer failed: ret = %d, tlen = %d\n", ret, tlen);
+
+	memcpy(&ret, buf, 4);
+	return ret;
+}
+
+static int plx_writereg(struct usb_plx *dev, u16 reg, u32 data)
+{
+	int ret = -EINVAL;
+	u32 csrctl = 0U; // Format p362
+	u32 *buf;
+	int tlen = 0;
+	unsigned int pipe;
+
+	pr_debug("reg=0x%x, data=0x%.8x\n",reg, data);
+
+	csrctl |= 0b1111; // CSR Bytes enable (all bytes)
+  csrctl |= 0b01 << 4; // select USB configuration registers
+  csrctl |= 1 << 6; // CSR Start
+  csrctl |= 0 << 7; // CSR write transaction
+  csrctl |= (uint32_t ) reg << 16; // destination address
+
+	buf = kmalloc(8, GFP_KERNEL);
+	if (!buf) {
+		pr_err("Can't allocate memory for USB buffer");
+		return -ENOMEM;
+	}
+	memcpy(buf, &csrctl, 4);
+	memcpy(buf+4, &data, 4);
+
+	pipe = usb_sndbulkpipe(dev->udev, dev->csr_out_endpointAddr);
+	ret = usb_bulk_msg(dev->udev, pipe, buf, 8, &tlen, 1000);
+
+	if (ret != 0 || tlen != 8)
+		pr_warn("USB out transfer failed: ret = %d, tlen = %d\n", ret, tlen);
+
+	return ret;
+}
+//
+// read memory from addr to the buffer provided by caller using DMA
+// engine
+static int dma_readbuf(struct usb_plx *dev, const uint32_t dmaaddr, unsigned char* buf, size_t count)
+{
+	int ret = -EINVAL;
+	u32 dmacount = 0;
+	u32 dmactl = dmactl_default;
+	u32 dmastat = 0;
+	int tlen = 0;
+	u32 tmp = 0;
+	u32 ep_avail = 0;
+	int pipe;
+
+	pr_debug("dev=%p, dmaaddr = 0x%.8x, buf=%p, count=%zu\n", dev, dmaaddr, buf, count);
+
+	// Configure DMA registers
+	plx_writereg(dev, REG_DMA1_CTL, dmactl); // start fresh
+	plx_writereg(dev, REG_GPEP1_STAT, BIT(USB_STALL_SENT));
+	plx_writereg(dev, REG_GPEP1_STAT, BIT(FIFO_FLUSH) | BIT(SHORT_PACKET_TRANSFERRED_INTERRUPT));
+
+	plx_writereg(dev, REG_DMA1_ADDR, dmaaddr);
+
+	//readreg(dev, REG_DMA1_COUNT, &dmacount);
+	dmacount |= count;
+	dmacount |= BIT(DMA_DIRECTION); // DMA Direction: IN (read)
+	dmacount |= BIT(END_OF_CHAIN);
+	dmacount |= BIT(VALID_BIT);
+	dmacount |= BIT(DMA_DONE_INTERRUPT_ENABLE);
+	plx_writereg(dev, REG_DMA1_COUNT, dmacount);
+
+	dmactl = plx_readreg(dev, REG_DMA1_CTL);
+	dmactl |= BIT(DMA_ENABLE); // DMA Enable
+	dmactl |= BIT(DMA_FIFO_VALIDATE); // Validate last short packet
+	plx_writereg(dev, REG_DMA1_CTL, dmactl);
+
+	// Start DMA transfer
+	dmastat = plx_readreg(dev, REG_DMA1_STAT);
+	dmastat |= BIT(DMA_START);
+	plx_writereg(dev, REG_DMA1_STAT, dmastat);
+
+	while((dmacount & 0x7fffff) && ep_avail) {
+		udelay(1000);
+		tmp = plx_readreg(dev, REG_DMA1_COUNT);
+		if(tmp != dmacount)
+		{
+			dmacount = tmp;
+			pr_debug("dmacount=0x%.8x", dmacount);
+		}
+
+		ep_avail = plx_readreg(dev, REG_GPEP1_AVAIL);
+		if(!ep_avail)
+			pr_warn("USB IN FIFO full");
+	}
+
+	pipe = usb_rcvbulkpipe(dev->udev, dev->bulk_in_endpointAddr);
+	ret = usb_bulk_msg(dev->udev, pipe, buf, count, &tlen, 1000);
+
+	if (ret != 0 || tlen != count)
+	{
+		pr_warn("DMA in transfer failed (ret=%d, tlen=%d, dmaaddr=0x%x)", ret, tlen, dmaaddr);
+
+		//readreg(dev, REG_GPEP1_RSP, &tmp);
+		//if (tmp & BIT(CLEAR_ENDPOINT_TOGGLE))
+
+		//readreg(dev, REG_GPEP1_STAT, &tmp);
+		//if (tmp & BIT(USB_STALL_SENT))
+			plx_writereg(dev, REG_GPEP1_STAT, BIT(USB_STALL_SENT));
+			plx_writereg(dev, REG_GPEP1_STAT, BIT(FIFO_FLUSH) | BIT(SHORT_PACKET_TRANSFERRED_INTERRUPT));
+			plx_writereg(dev, REG_GPEP1_RSP, BIT(CLEAR_ENDPOINT_TOGGLE));
+	}
+
+	return ret;
+}
+// write memory to addr from the buffer provided by caller using DMA engine
+static int dma_writebuf(struct usb_plx *dev, const uint32_t dmaaddr, unsigned char* buf, size_t count)
+{
+	int ret = -EINVAL;
+	u32 dmacount = 0;
+	u32 dmactl = dmactl_default;
+	u32 dmastat = 0;
+	int tlen = 0;
+	int pipe;
+
+	pr_debug("dev=%p, dmaaddr = 0x%.8x, buf=%p, count=%zu\n", dev, dmaaddr, buf, count);
+
+	// Configure DMA registers
+	plx_writereg(dev, REG_DMA0_CTL, dmactl); // start fresh
+	plx_writereg(dev, REG_GPEP0_STAT, BIT(USB_STALL_SENT));
+	plx_writereg(dev, REG_GPEP0_STAT, BIT(FIFO_FLUSH) | BIT(SHORT_PACKET_TRANSFERRED_INTERRUPT));
+
+	plx_writereg(dev, REG_DMA0_ADDR, dmaaddr);
+
+	//readreg(dev, REG_DMA0_COUNT, &dmacount);
+	dmacount |= count;
+	//dmacount |= BIT(DMA_DIRECTION); // DMA Direction: OUT (write)
+	dmacount |= BIT(END_OF_CHAIN);
+	dmacount |= BIT(VALID_BIT);
+	dmacount |= BIT(DMA_DONE_INTERRUPT_ENABLE);
+	plx_writereg(dev, REG_DMA0_COUNT, dmacount);
+
+	dmactl = plx_readreg(dev, REG_DMA1_CTL);
+	dmactl |= BIT(DMA_ENABLE); // DMA Enable
+	dmactl |= BIT(DMA_FIFO_VALIDATE); // Validate last short packet
+	plx_writereg(dev, REG_DMA0_CTL, dmactl);
+
+	// Start DMA transfer
+	dmastat = plx_readreg(dev, REG_DMA0_STAT);
+	dmastat |= BIT(DMA_START);
+	plx_writereg(dev, REG_DMA0_STAT, dmastat);
+
+#if 0
+	while((dmacount & 0x7fffff) && ep_avail) {
+		udelay(1000);
+		tmp = plx_readreg(dev, REG_DMA0_COUNT);
+		if(tmp != dmacount)
+		{
+			dmacount = tmp;
+			pr_debug("dmacount=0x%.8x", dmacount);
+		}
+
+		ep_avail = plx_readreg(dev, REG_GPEP0_AVAIL);
+		if(!ep_avail)
+			pr_warn("USB IN FIFO full");
+	}
+#endif
+
+	pipe = usb_sndbulkpipe(dev->udev, dev->bulk_out_endpointAddr);
+	ret = usb_bulk_msg(dev->udev, pipe, buf, count, &tlen, 1000);
+
+	if (ret != 0 || tlen != count)
+	{
+		pr_warn("DMA in transfer failed (ret=%d, tlen=%d, dmaaddr=0x%x)", ret, tlen, dmaaddr);
+
+		//readreg(dev, REG_GPEP1_RSP, &tmp);
+		//if (tmp & BIT(CLEAR_ENDPOINT_TOGGLE))
+
+		//readreg(dev, REG_GPEP1_STAT, &tmp);
+		//if (tmp & BIT(USB_STALL_SENT))
+			plx_writereg(dev, REG_GPEP0_STAT, BIT(USB_STALL_SENT));
+			plx_writereg(dev, REG_GPEP0_STAT, BIT(FIFO_FLUSH) | BIT(SHORT_PACKET_TRANSFERRED_INTERRUPT));
+			plx_writereg(dev, REG_GPEP0_RSP, BIT(CLEAR_ENDPOINT_TOGGLE));
+	}
+
+	return ret;
+}
+
+static ssize_t usbmem_read(struct file *file, char *buffer, size_t count,
+			 loff_t *ppos)
+{
+	struct usb_plx *dev;
+	int rv;
+
+	if (count > DMA_BUFLEN)
+		return -EINVAL;
+
+	dev = file->private_data;
+	pr_debug("dev=%p, ppos=%p, *ppos=%llx\n", dev, ppos, *ppos);
+	rv = dma_readbuf(dev, *ppos, dev->bulk_in_buffer, count);
+	pr_debug("dma_readbuf: rv=%d\n", rv);
+	if (rv < 0)
+		return rv;
+	//print_hex_dump(KERN_DEBUG, "buf: ", DUMP_PREFIX_OFFSET, 16, 1, dev->bulk_in_buffer, count, true);
+	if (copy_to_user(buffer, dev->bulk_in_buffer, count))
+		rv = -EFAULT;
+	else
+	{
+		rv = count;
+		*ppos += count;
+	}
+	return rv;
+}
+
+static ssize_t usbmem_write(struct file *file, const char *user_buffer,
+			  size_t count, loff_t *ppos)
+{
+	struct usb_plx *dev;
+	int rv = 0;
+
+	/* verify that we actually have some data to write */
+	if (count == 0 || count > DMA_BUFLEN)
+		return -EINVAL;
+
+	dev = file->private_data;
+	pr_debug("dev=%p, ppos=%p, *ppos=%llx\n", dev, ppos, *ppos);
+	if (copy_from_user(dev->bulk_out_buffer, user_buffer, count))
+		rv = -EFAULT;
+	print_hex_dump(KERN_DEBUG, "buf: ", DUMP_PREFIX_OFFSET, 16, 1, dev->bulk_out_buffer, count, true);
+	rv = dma_writebuf(dev, *ppos, dev->bulk_out_buffer, count);
+	pr_debug("dma_writebuf: rv=%d\n", rv);
+	if (rv < 0)
+		return rv;
+	else
+	{
+		rv = count;
+		*ppos += count;
+	}
+	return rv;
+}
+
+static int plx_probe(struct usb_interface *interface,
+		      const struct usb_device_id *id)
+{
+	struct usb_plx *dev;
+	struct usb_host_interface *iface_desc;
+	struct usb_endpoint_descriptor *endpoint;
+	size_t buffer_size;
+	int i;
+	int retval = -ENOMEM;
+
+	/* allocate memory for our device state and initialize it */
+	dev = kzalloc(sizeof(*dev), GFP_KERNEL);
+	if (!dev) {
+		dev_err(&interface->dev, "Out of memory\n");
+		goto error;
+	}
+	kref_init(&dev->kref);
+	sema_init(&dev->limit_sem, WRITES_IN_FLIGHT);
+	mutex_init(&dev->io_mutex);
+	spin_lock_init(&dev->err_lock);
+	init_usb_anchor(&dev->submitted);
+	init_waitqueue_head(&dev->bulk_in_wait);
+
+	dev->udev = usb_get_dev(interface_to_usbdev(interface));
+	dev->interface = interface;
+
+	/* set up the endpoint information */
+	/* use only the first bulk-in and bulk-out endpoints */
+	iface_desc = interface->cur_altsetting;
+	for (i = 0; i < iface_desc->desc.bNumEndpoints; ++i) {
+		endpoint = &iface_desc->endpoint[i].desc;
+
+		if (!dev->bulk_in_endpointAddr &&
+		    usb_endpoint_is_bulk_in(endpoint) &&
+				endpoint->bEndpointAddress & PLX_DMA_ENDPOINT) {
+			/* we found the DMA bulk in endpoint */
+			buffer_size = usb_endpoint_maxp(endpoint);
+			dev->bulk_in_size = buffer_size;
+			dev->bulk_in_endpointAddr = endpoint->bEndpointAddress;
+			dev->bulk_in_buffer = kzalloc(DMA_BUFLEN, GFP_KERNEL);
+			if (!dev->bulk_in_buffer) {
+				dev_err(&interface->dev,
+					"Could not allocate bulk_in_buffer\n");
+				goto error;
+			}
+		}
+		if (!dev->bulk_out_endpointAddr &&
+		    usb_endpoint_is_bulk_out(endpoint) &&
+				endpoint->bEndpointAddress & PLX_DMA_ENDPOINT) {
+			/* we found the DMA bulk out endpoint */
+			buffer_size = usb_endpoint_maxp(endpoint);
+			dev->bulk_out_size = buffer_size;
+			dev->bulk_out_endpointAddr = endpoint->bEndpointAddress;
+			dev->bulk_out_buffer = kzalloc(DMA_BUFLEN, GFP_KERNEL);
+			if (!dev->bulk_out_buffer) {
+				dev_err(&interface->dev,
+					"Could not allocate bulk_out_buffer\n");
+				goto error;
+			}
+		}
+
+		if (!dev->csr_in_endpointAddr &&
+		    usb_endpoint_is_bulk_in(endpoint) &&
+				endpoint->bEndpointAddress & PLX_CSR_ENDPOINT) {
+			/* we found the CSR bulk in endpoint */
+			dev->csr_in_endpointAddr = endpoint->bEndpointAddress;
+		}
+		if (!dev->csr_out_endpointAddr &&
+		    usb_endpoint_is_bulk_out(endpoint) &&
+				endpoint->bEndpointAddress & PLX_CSR_ENDPOINT) {
+			/* we found the CSR bulk out endpoint */
+			dev->csr_out_endpointAddr = endpoint->bEndpointAddress;
+		}
+
+	}
+	if (!(dev->bulk_in_endpointAddr && dev->bulk_out_endpointAddr)) {
+		dev_err(&interface->dev,
+			"Could not find both bulk-in and bulk-out endpoints\n");
+		goto error;
+	}
+	pr_debug("DMA endpoints: 0x%x/0x%x - 0x%x/0x%x\n",
+		 dev->bulk_in_endpointAddr,
+		 dev->bulk_out_endpointAddr,
+		 dev->csr_in_endpointAddr,
+		 dev->csr_out_endpointAddr);
+
+	/* save our data pointer in this interface device */
+	usb_set_intfdata(interface, dev);
+
+	/* we can register the device now, as it is ready */
+	retval = usb_register_dev(interface, &plx_class);
+	if (retval) {
+		/* something prevented us from registering this driver */
+		dev_err(&interface->dev,
+			"Not able to get a minor for this device.\n");
+		usb_set_intfdata(interface, NULL);
+		goto error;
+	}
+
+	/* let the user know what node this device is now attached to */
+	dev_info(&interface->dev,
+		 "PLX device now attached to usbmem-%d",
+		 interface->minor);
+	return 0;
+
+error:
+	if (dev)
+		/* this frees allocated memory */
+		kref_put(&dev->kref, plx_delete);
+	return retval;
+}
+
+static void plx_disconnect(struct usb_interface *interface)
+{
+	struct usb_plx *dev;
+	int minor = interface->minor;
+
+	dev = usb_get_intfdata(interface);
+	usb_set_intfdata(interface, NULL);
+
+	/* give back our minor */
+	usb_deregister_dev(interface, &plx_class);
+
+	/* prevent more I/O from starting */
+	mutex_lock(&dev->io_mutex);
+	dev->interface = NULL;
+	mutex_unlock(&dev->io_mutex);
+
+	usb_kill_anchored_urbs(&dev->submitted);
+
+	/* decrement our usage count */
+	kref_put(&dev->kref, plx_delete);
+
+	dev_info(&interface->dev, "usbmem #%d now disconnected", minor);
+}
+
+static void plx_draw_down(struct usb_plx *dev)
+{
+	int time;
+
+	time = usb_wait_anchor_empty_timeout(&dev->submitted, 1000);
+	if (!time)
+		usb_kill_anchored_urbs(&dev->submitted);
+}
+
+static int plx_suspend(struct usb_interface *intf, pm_message_t message)
+{
+	struct usb_plx *dev = usb_get_intfdata(intf);
+
+	if (!dev)
+		return 0;
+	plx_draw_down(dev);
+	return 0;
+}
+
+static int skel_resume(struct usb_interface *intf)
+{
+	return 0;
+}
+
+static int skel_pre_reset(struct usb_interface *intf)
+{
+	struct usb_plx *dev = usb_get_intfdata(intf);
+
+	mutex_lock(&dev->io_mutex);
+	plx_draw_down(dev);
+
+	return 0;
+}
+
+static int plx_post_reset(struct usb_interface *intf)
+{
+	struct usb_plx *dev = usb_get_intfdata(intf);
+
+	/* we are sure no URBs are active - no locking needed */
+	dev->errors = -EPIPE;
+	mutex_unlock(&dev->io_mutex);
+
+	return 0;
+}
+
+
+
+module_usb_driver(plx_driver);
+
+MODULE_LICENSE("GPL");
+
---
+++ src/pciemem.h
@@ -0,0 +1,132 @@
+#include <linux/usb.h>
+#include <linux/fs.h>
+#include <linux/slab.h>
+#include <linux/bitops.h>
+#include <linux/usb/net2280.h>
+#include <linux/uaccess.h>
+
+#define PLX_VENDOR_ID 0x0525
+#define PLX_PRODUCT_ID 0x3380
+#define PLX_DEVICE_NAME "usbmem"
+
+/* USB part */
+#define PLX_PCI_ENDPOINT 0xe
+#define PLX_CSR_ENDPOINT 0xd
+#define PLX_DMA_ENDPOINT 0x2
+
+/* General Puropose Endpoint registers */
+#define REG_GPEP0_CFG		0x320
+#define REG_GPEP0_RSP		0x324 // CHECK!
+#define REG_GPEP0_STAT	0x32c
+#define REG_GPEP0_AVAIL	0x330
+#define REG_GPEP1_CFG		0x340
+#define REG_GPEP1_RSP		0x344
+#define REG_GPEP1_STAT	0x40c
+#define REG_GPEP1_AVAIL	0x410
+
+/* DMA registers */
+#define REG_DMA0_CTL		0x180
+#define REG_DMA0_STAT		0x184
+#define REG_DMA0_COUNT	0x190
+#define REG_DMA0_ADDR		0x194
+
+#define REG_DMA1_CTL		0x1A0
+#define REG_DMA1_STAT		0x1A4
+#define REG_DMA1_COUNT	0x1B0
+#define REG_DMA1_ADDR		0x1B4
+
+static const u32 dmactl_default =
+		  (1 << DMA_SCATTER_GATHER_DONE_INTERRUPT)
+		| (1 << DMA_CLEAR_COUNT_ENABLE)
+		/* erratum 0116 workaround part 1 (use POLLING) */
+		| (POLL_100_USEC << DESCRIPTOR_POLLING_RATE)
+		| (1 << DMA_VALID_BIT_POLLING_ENABLE)
+		| (1 << DMA_VALID_BIT_ENABLE)
+		//| (1 << DMA_SCATTER_GATHER_ENABLE)
+		/* erratum 0116 workaround part 2 (no AUTOSTART) */
+		| (1 << DMA_ENABLE);
+
+/* Get a minor range for your devices from the usb maintainer */
+#define USB_PLX_MINOR_BASE	192
+#define WRITES_IN_FLIGHT	8
+
+#define MAX_TRANSFER		(PAGE_SIZE - 512)
+/* MAX_TRANSFER is chosen so that the VM is not stressed by
+   allocations > PAGE_SIZE and the number of packets in a page
+   is an integer 512 is the largest possible packet on EHCI */
+
+#define DMA_BUFLEN 4 * 1024 * 1024
+static int plx_probe(struct usb_interface *interface, const struct usb_device_id *id);
+static void plx_disconnect(struct usb_interface *interface);
+static int plx_suspend(struct usb_interface *intf, pm_message_t message);
+static int plx_post_reset(struct usb_interface *intf);
+
+/* direct copy from usb-skeleton.c */
+static int skel_resume(struct usb_interface *intf);
+static int skel_pre_reset(struct usb_interface *intf);
+
+static struct usb_device_id plx_table [] = {
+	    { USB_DEVICE(PLX_VENDOR_ID, PLX_PRODUCT_ID) },
+			    { }
+};
+static struct usb_driver plx_driver = {
+  .name = PLX_DEVICE_NAME,
+  .id_table = plx_table,
+  .probe = plx_probe,
+  .disconnect = plx_disconnect,
+	.suspend =	plx_suspend,
+	.resume =	skel_resume,
+	.pre_reset =	skel_pre_reset,
+	.post_reset =	plx_post_reset,
+	.supports_autosuspend = 1,
+};
+
+static ssize_t usbmem_read(struct file *, char *, size_t, loff_t *);
+static ssize_t usbmem_write(struct file *, const char *, size_t, loff_t *);
+static int usbmem_open(struct inode *, struct file *);
+static int usbmem_release(struct inode *, struct file *);
+static int usbmem_flush(struct file *file, fl_owner_t id);
+
+static struct file_operations fops = {
+	.owner = THIS_MODULE,
+  .read = usbmem_read,
+  .write = usbmem_write,
+  .open = usbmem_open,
+  .release = usbmem_release,
+	.flush =	usbmem_flush,
+	.llseek =	generic_file_llseek,
+};
+
+static struct usb_class_driver plx_class = {
+	.name = PLX_DEVICE_NAME"%d",
+	.fops = &fops,
+	.minor_base =	USB_PLX_MINOR_BASE,
+};
+
+struct usb_plx {
+	struct usb_device	*udev;			/* the usb device for this device */
+	struct usb_interface	*interface;		/* the interface for this device */
+	struct semaphore	limit_sem;		/* limiting the number of writes in progress */
+	struct usb_anchor	submitted;		/* in case we need to retract our submissions */
+	unsigned char           *bulk_in_buffer;	/* the buffer to receive data */
+	size_t			bulk_in_size;		/* the size of the receive buffer */
+	size_t			bulk_in_filled;		/* number of bytes in the buffer */
+	size_t			bulk_in_copied;		/* already copied to user space */
+	__u8			bulk_in_endpointAddr;	/* the address of the bulk in endpoint */
+	unsigned char           *bulk_out_buffer;	/* the buffer to transmit data */
+	size_t			bulk_out_size;		/* the size of the receive buffer */
+	__u8			bulk_out_endpointAddr;	/* the address of the bulk out endpoint */
+	__u8			csr_in_endpointAddr;	/* the address of the CSR in endpoint */
+	__u8			csr_out_endpointAddr;	/* the address of the CSR out endpoint */
+	int			errors;			/* the last request tanked */
+	bool			ongoing_read;		/* a read is going on */
+	spinlock_t		err_lock;		/* lock for errors */
+	struct kref		kref;
+	struct mutex		io_mutex;		/* synchronize I/O with disconnect */
+	wait_queue_head_t	bulk_in_wait;		/* to wait for an ongoing read */
+};
+
+static void plx_draw_down(struct usb_plx *dev);
+
+static int plx_readreg(struct usb_plx *dev, u16 reg);
+#define to_plx_dev(d) container_of(d, struct usb_plx, kref)
